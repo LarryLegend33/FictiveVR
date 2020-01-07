faa8ec210bf5cffb8d6fc673e186c514fd605fb9
Andrew D Bolton <andrewdbolton@fas.harvard.edu>
2020-01-07 10:57:55 -0500

    First Commit

---
+++ PipeServer/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+using System.IO.Pipes;
+using System.Diagnostics;
+
+class PipeServer
+{
+    static void Main()
+    {
+        Process pipeClient = new Process();
+
+        pipeClient.StartInfo.FileName = "C:/Users/martinH/Desktop/BuiltUnityGame/5cube2fish.exe";
+        int counter = 0; 
+        using (AnonymousPipeServerStream pipeServer =
+            new AnonymousPipeServerStream(PipeDirection.Out,
+            HandleInheritability.Inheritable))
+        {
+            Console.WriteLine("[SERVER] Current TransmissionMode: {0}.",
+                pipeServer.TransmissionMode);
+
+            // Pass the client process a handle to the server.
+            pipeClient.StartInfo.Arguments =
+                pipeServer.GetClientHandleAsString();
+            pipeClient.StartInfo.UseShellExecute = false;
+            pipeClient.Start();
+
+            pipeServer.DisposeLocalCopyOfClientHandle();
+
+            try
+            {
+                // Read user input and send that to the client process.
+                using (StreamWriter sw = new StreamWriter(pipeServer))
+                {
+                    // sw.AutoFlush = true;
+                    sw.AutoFlush = false;
+                    Console.WriteLine("Before Loop");
+                    while (counter < 200000)
+                    {
+                        //sw.AutoFlush = true;
+                        // Send a 'sync message' and wait for client to receive it.
+                        sw.WriteLine(counter.ToString());
+                        Console.WriteLine("Writing Sample" + counter.ToString());
+                        pipeServer.WaitForPipeDrain();
+                        counter++;
+                        // Send the console input to the client process.
+                       // Console.Write("[SERVER] Enter text: ");
+                       // sw.WriteLine(Console.ReadLine());
+                    }
+                    Console.WriteLine("Wrote 200K Samples");
+                }
+            }
+            // Catch the IOException that is raised if the pipe is broken
+            // or disconnected.
+            catch (IOException e)
+            {
+                Console.WriteLine("[SERVER] Error: {0}", e.Message);
+            }
+        }
+
+        pipeClient.WaitForExit();
+        pipeClient.Close();
+        Console.WriteLine("[SERVER] Client quit. Server terminating.");
+    }
+}
---
+++ unityfisheyesample2/unityfisheyesample/Assets/PlayerController.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.IO.Pipes;
+using System.Threading;
+using System; 
+
+
+public class PipeClientCreator
+{
+    public string tail_voltage, pipe_handle;
+    public Tuple<float, float> tail_command; 
+
+    public PipeClientCreator(string handle)
+    {
+        tail_voltage = "left_startval";
+        pipe_handle = handle;
+        tail_command = new Tuple<float, float>(0, 0);
+    }
+
+    public void VoltageParser()
+    {
+        string[] parsed_voltage = tail_voltage.Split(',');
+        tail_command = new Tuple<float, float>(float.Parse(parsed_voltage[0]), float.Parse(parsed_voltage[1]));
+    }
+
+    public void StartClient()
+        {
+            using (PipeStream pipeClient =
+                new AnonymousPipeClientStream(PipeDirection.In, pipe_handle))
+            {
+                using (StreamReader sr = new StreamReader(pipeClient))
+                {
+                // Read the server data and echo to the console.
+                    while (true)
+                    {
+                       // tail_left = counter.ToString();
+                        if (sr.Peek() >= 0)
+                        {
+                        //counter++;
+                            tail_voltage = sr.ReadLine();
+                            VoltageParser(); 
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+
+
+public class PlayerController : MonoBehaviour
+{
+
+    private float translational_gain = 15.0f;
+    private float rotational_gain = 45.0f;
+    private float horizontalInput, forwardInput;
+    private string[] cmdLineArgs;
+    public string pipe_input = "start";
+    public PipeClientCreator pCC; 
+    
+   
+    // Note that if you make this public, unity takes it on as a public variable.
+    // It then cannot be set in here. 
+
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        cmdLineArgs = System.Environment.GetCommandLineArgs(); 
+        pCC = new PipeClientCreator(cmdLineArgs[1]);
+        var pipeThread = new Thread(pCC.StartClient);
+        pipeThread.Start(); 
+    }
+
+    void OnGUI()
+    {
+        try
+        {
+            GUI.Label(new Rect(10, 10, 100, 20), "T: " + translational_gain.ToString());
+            GUI.Label(new Rect(10, 50, 100, 20), "R: " + rotational_gain.ToString());
+        }
+
+        catch 
+        {
+
+            GUI.Label(new Rect(10, 10, 100, 20), "Exception in OnGUI");
+        }
+     // GUI.Label(new Rect(40, 40, 100, 20), cmdLineArgs[0]);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            translational_gain += 1f;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            translational_gain -= 1f;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rotational_gain += 1f;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rotational_gain -= 1f;
+        }
+       // translational_gain += Input.GetAxis("Vertical");
+       // rotational_gain += Input.GetAxis("Horizontal");
+        horizontalInput = (float)(Math.Abs(pCC.tail_command.Item2) - Math.Abs(pCC.tail_command.Item1)) * rotational_gain;
+        //  horizontalInput = Input.GetAxis("Horizontal");
+        //   forwardInput = Input.GetAxis("Vertical");
+        forwardInput = (float)Math.Sqrt(Math.Pow(pCC.tail_command.Item2, 2) + Math.Pow(pCC.tail_command.Item1, 2)) * translational_gain; 
+        // moves at 1 unit per second
+        transform.Translate(Vector3.forward * Time.deltaTime * forwardInput);
+        transform.Rotate(Vector3.up * Time.deltaTime * horizontalInput);
+    }
+}
